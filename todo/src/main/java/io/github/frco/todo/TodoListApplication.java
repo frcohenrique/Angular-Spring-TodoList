cdbf3e05b22abfcd4b46f9f22402a9a6dae4dea7
Henrique Franco <henrique.frco@gmail.com>
2022-05-27 18:44:43 -0300

    Updating backend

---
+++ todo/src/main/java/io/github/frco/todo/TodoListApplication.java
@@ -0,0 +1,13 @@
+package io.github.frco.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplication {	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplication.class, args);
+	}
+
+}
